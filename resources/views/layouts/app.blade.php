62ef58d68f2889d42e66434bfe9ab2fc17af31c9
mrushidany <mrushidany@gmail.com>
2022-09-26 19:02:37 +0300

    title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>NCBA | M-PAWA DRAW</title>
+    <title>RAFFLE | TOOL</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
