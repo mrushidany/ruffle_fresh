1051c5c2174fef1eda67bf9f8acfd80eb120bef4
mrushidany <mrushidany@gmail.com>
2022-09-27 11:24:34 +0300

    Login form styling

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,13 +3,13 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8 mt-5">
+        <div class="col-md-6 mt-5">
             <div class="card login-card">
 
                 <div class="card-body">
                     <div class="row">
                         <div class="col-md-12">
-                            <p class="login-greeting">M-PAWA DRAW</p>
+                            <p class="login-greeting offset-md-4">RAFFLE TOOL</p>
                             <p class="login-description">Please login to your dashboard to continue</p>
                         </div>
                     </div>
@@ -19,7 +19,7 @@
                         <div class="form-group row">
                             {{-- <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label> --}}
 
-                            <div class="col-md-10" style="position: relative">
+                            <div class="col-md-12" style="position: relative">
                                 <img src="{{asset('images/username.png')}}" class="login-input-icon">
                                 <input id="email" type="email" class="form-control login-input @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="Enter your email">
 
@@ -34,7 +34,7 @@
                         <div class="form-group row">
                             {{-- <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label> --}}
 
-                            <div class="col-md-10">
+                            <div class="col-md-12">
                                 <img src="{{asset('images/password.png')}}" class="login-input-icon">
                                 <input id="password" type="password" class="form-control login-input @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" placeholder="Enter your password">
 
@@ -46,8 +46,8 @@
                             </div>
                         </div>
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-10">
+                        <div class="form-group row mb-0 mt-4">
+                            <div class="col-md-12">
                                 <button type="submit" class="btn btn-primary btn-block py-3 cta-button">
                                     {{ __('Login') }}
                                 </button>
