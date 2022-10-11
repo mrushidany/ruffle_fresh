930f0c5bf4e716e7f87cfc6233b62e38408b5aac
mrushidany <mrushidany@gmail.com>
2022-10-11 12:45:47 +0300

    Styling landing page

--- resources/views/landing_page.blade.php
+++ resources/views/landing_page.blade.php
@@ -9,9 +9,23 @@
     {{-- Styles --}}
     <link rel="stylesheet" href="css/main.css">
 
+      <!-- font awesome cdn link -->
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+
     <title>RAFFLE | TOOL</title>
 </head>
-<body>
-    
+<body class="main-bg">
+   <div class="home-bg">
+        <section class="home">
+            <div class="block">
+                 <h3 class="">RAFFLE TOOL DEMO</h3>  
+                 <p>To proceed to the Raffle Tool Demo, click the proceed button below</p> 
+                 <button>Proceed</button>
+            </div>
+            <div class="lottery-img">
+                <img src="{{ asset('images/jackpot-lottery.png')}}" style="width: 50em;" class="bounce" />
+            </div>
+        </section>
+   </div>
 </body>
 </html>
